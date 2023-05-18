796c181eaaeacc257ef11ac05858433350e1ff6c
mszrsnori <113504925+mszrsnori@users.noreply.github.com>
2023-05-18 12:53:57 +0200

    Add files via upload

---
+++ xx2.py
@@ -0,0 +1,13 @@
+a = int(input("Írj be egy számot 5 és 10 között! :"))
+if a == 5:
+    print("fröccsöt")
+elif a == 6:
+    print("sírhat")
+elif a == 7:
+    print("Porschét")
+elif a == 8:
+    print("Miskolc!")
+elif a == 9:
+    print("bukfenc")
+elif a == 10:
+    print("sztriptíz")
